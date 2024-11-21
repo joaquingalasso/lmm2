034068468f98b9f5a59f623ea8791cf643462675
Joaquín Galasso <joacogalasso@gmail.com>
2024-11-21 09:45:05 -0300

    Cambio de mecánica de desplazamiento
    .

--- entrega/micelio.js
+++ entrega/micelio.js
@@ -10,6 +10,23 @@ class micelio {
     this.cuentaTecla = 1;
       this.estela = []; // Lista para las posiciones del rastro
   this.maxEstela = 500; // Cantidad máxima de puntos en el rastro
+
+  // Arrays de teclas agrupadas por dirección
+  this.teclasArriba = ['5', '6', '7', '8', 't', 'y', 'u', 'i', 'g', 'h', 'j', 'k', 
+                     '5', '6', '7', '8', 'T', 'Y', 'U', 'I', 'G', 'H', 'J', 'K'];
+this.teclasAbajo = ['<', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', ' ', '.', '-', 
+                    '<', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', ' ', '.', '-'];
+this.teclasIzquierda = ['a', 's', 'z', 'q', '|', '1', '2', '3', '4', 'q', 'w', 'e', 'r', 'a', 's', 'd', 'f', 'TAB',
+                        'A', 'S', 'Z', 'Q', '|', '1', '2', '3', '4', 'Q', 'W', 'E', 'R', 'A', 'S', 'D', 'F', 'TAB'];
+this.teclasDerecha = ['9', '0', '\'', '¿', 'o', 'p', "Dead", '+', 'l', 'ñ', '{', '}', 'ENTER', 'BACKSPACE',
+                      '9', '0', '\'', '¿', 'O', 'P', "Dead", '+', 'L', 'Ñ', '{', '}', 'ENTER', 'BACKSPACE'];
+
+    
+   // Contador de pulsaciones para rotar arrays
+   this.contadorTeclas = {};
+   [...this.teclasArriba, ...this.teclasAbajo, ...this.teclasIzquierda, ...this.teclasDerecha].forEach(tecla => {
+     this.contadorTeclas[tecla] = 0;
+   });
   }
 
   dibujar() {
@@ -71,34 +88,55 @@ dibujarEstela() {
     }
   }
 }
+
+ // Función para rotar los arrays de teclas
+ rotarArrays() {
+  let temp = this.teclasArriba;
+  this.teclasArriba = this.teclasIzquierda;
+  this.teclasIzquierda = this.teclasAbajo;
+  this.teclasAbajo = this.teclasDerecha;
+  this.teclasDerecha = temp;
+}
+
 crece(direccion) {
   if (!this.primeraTecla) {
     this.primeraTecla = true;
+  }
+
+  // Actualizar contador de tecla
+  this.contadorTeclas[direccion] = (this.contadorTeclas[direccion] || 0) + 1;
+
+  // Rotar arrays si la misma tecla se presiona 3 veces
+  if (this.contadorTeclas[direccion] === 3) {
+    this.rotarArrays();
+    // Reiniciar contadores
+    Object.keys(this.contadorTeclas).forEach(tecla => {
+      this.contadorTeclas[tecla] = 0;
+    });
   }
 
   let ultimaPos = this.guardaPos[this.guardaPos.length - 1];
   let nuevaPos = { x: ultimaPos.x, y: ultimaPos.y };
   let randomAngle = random(-this.angleChangeAmt, this.angleChangeAmt);
 
-  // Movimiento basado en la dirección
-  if (direccion === 's' || direccion === 'w' || direccion === 'x' || direccion === 'a' || direccion === 'q' || direccion === 'z') {
+  // Movimiento basado en los arrays
+  if (this.teclasArriba.includes(direccion)) {
+    nuevaPos.y -= 20 * cos(randomAngle);
+  } else if (this.teclasAbajo.includes(direccion)) {
+    nuevaPos.y += 20 * cos(randomAngle);
+  } else if (this.teclasIzquierda.includes(direccion)) {
     nuevaPos.x -= 20 * cos(randomAngle);
     nuevaPos.y -= 20 * sin(randomAngle);
-  } else if (direccion === 'r' || direccion === 't' || direccion === 'y' || direccion === 'u' || direccion === 'g' || direccion === 'f') {
-    nuevaPos.y -= 20 * cos(randomAngle);
-  } else if (direccion === 'k' || direccion === 'l' || direccion === 'o' || direccion === 'i') {
+  } else if (this.teclasDerecha.includes(direccion)) {
     nuevaPos.x += 20 * cos(randomAngle);
     nuevaPos.y += 20 * sin(randomAngle);
-  } else if (direccion === 'c' || direccion === 'v' || direccion === 'b' || direccion === 'n' || direccion === 'm') {
-    nuevaPos.y += 20 * cos(randomAngle);
   }
 
- nuevaPos.x = constrain(nuevaPos.x, 0+20, width-20);
- nuevaPos.y = constrain(nuevaPos.y, 270, height);
- this.cuentaTecla++;
+  nuevaPos.x = constrain(nuevaPos.x, 0 + 20, width - 20);
+  nuevaPos.y = constrain(nuevaPos.y, 270, height);
+  this.cuentaTecla++;
   this.guardaPos.push(nuevaPos);
   this.CuentaPresionesDeTecla++;
-  
 
   // Crear rama cada 5 movimientos
   if (this.CuentaPresionesDeTecla === 5) {
@@ -106,6 +144,7 @@ crece(direccion) {
     this.CuentaPresionesDeTecla = 0;
   }
 }
+
   crearRama(pos) {
     let direccionX = random([-20, 50]);
     let direccionY = random([-20, 50]);
